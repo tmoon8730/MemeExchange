9a59b1ce6ab2111bf41961f42c2a7c1b4326299b
Tyler Moon <moon.tyler@gmail.com>
2016-09-30 17:19:17 -0400

    Got session vars working
    having issues with the iframes but that is due to a restriction on Google’s side

--- imports/ui/components/searchForm.js
+++ imports/ui/components/searchForm.js
@@ -1,19 +1,33 @@
 import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
 import './searchForm.html';
 
-var searchTerm = "";
+
+this.searchTerm = "";
 
 Template.searchForm.events({
   'submit form': function(event){
     event.preventDefault();
     searchTerm = event.target.playerName.value;
     console.log(searchTerm);
+    Session.set("searchTerm", searchTerm);
   },
 })
 
 Template.searchForm.helpers({
   generateURL: function(){
-    console.log(searchTerm);
-    return "http://www.google.com/trends/fetchComponent?hl=en-US&q=pepe%20the%20frog,dat%20boi&cid=TIMESERIES_GRAPH_0&export=5&h=500";
-  }
+    if(Session.get("searchTerm") !== undefined || Session.get("searchTerm") !== ""){
+          var term = Session.get("searchTerm").replace(' ', "%20");
+    }
+    console.log("term = " + term);
+    var url = "http://www.google.com/trends/fetchComponent?hl=en-US&q=" + term + "&cid=TIMESERIES_GRAPH_0&export=5&h=500";
+    Session.set("searchURL",url);
+    return url;
+  },
+  searchTerm: function(){
+    return Session.get("searchTerm");
+  },
+  fullURL: function(){
+    return Session.get("searchURL");
+  },
 })
